bbf45f76c469c331be5e057c3e9e0d3569d42877
vignesh prabhu <vignesh.p@ninestars.in>
2018-12-05 14:43:46 +0530

    feat : add the foloowing 1.Add anim, drawable, colors, strings 2.change kotlin version to 3.0

--- app/src/main/java/com/ns/stellarjet/HomeActivity.java
+++ app/src/main/java/com/ns/stellarjet/HomeActivity.java
@@ -1,7 +1,7 @@
 package com.ns.stellarjet;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
