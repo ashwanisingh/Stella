eb4e98dc388b50979652748b002f1e97df116480
Vignesh Prabhu <sekarvprabhu@gmail.com>
2018-12-06 12:10:52 +0530

    feat : add OTP activity and UI

---
+++ app/src/main/java/com/ns/stellarjet/login/OTPActivity.java
@@ -0,0 +1,14 @@
+package com.ns.stellarjet.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.ns.stellarjet.R;
+
+public class OTPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp);
+    }
+}
