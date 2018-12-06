20694bdaca299265b02d2ffd0601379ebb81c99a
Vignesh Prabhu <sekarvprabhu@gmail.com>
2018-12-06 16:13:56 +0530

    feat :add CalendarActivity DataBinding

--- app/src/main/java/com/ns/stellarjet/booking/CalendarActivity.java
+++ app/src/main/java/com/ns/stellarjet/booking/CalendarActivity.java
@@ -1,14 +1,23 @@
 package com.ns.stellarjet.booking;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import com.ns.stellarjet.R;
+import com.ns.stellarjet.databinding.ActivityCalendarBinding;
 
 public class CalendarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calendar);
+        // obtain Binding
+        ActivityCalendarBinding activityCalendarBinding = DataBindingUtil.setContentView(this, R.layout.activity_calendar);
+
+        activityCalendarBinding.buttonScheduleBack.setOnClickListener(v -> onBackPressed());
+
+        activityCalendarBinding.buttonScheduleConfirmDate.setOnClickListener(v -> {
+
+        });
     }
 }
