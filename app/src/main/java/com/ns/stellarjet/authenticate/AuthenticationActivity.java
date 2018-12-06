2ad20a5de4ad4e8744385f73154385214c6e20ed
Vignesh Prabhu <sekarvprabhu@gmail.com>
2018-12-06 12:16:39 +0530

    faet : add TouchId activity and UI

---
+++ app/src/main/java/com/ns/stellarjet/authenticate/AuthenticationActivity.java
@@ -0,0 +1,14 @@
+package com.ns.stellarjet.authenticate;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.ns.stellarjet.R;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+    }
+}
