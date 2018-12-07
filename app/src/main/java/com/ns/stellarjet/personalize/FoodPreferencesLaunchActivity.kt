bde69edbeddb64a017f852834f7b0b9a8805552b
Vignesh Prabhu <sekarvprabhu@gmail.com>
2018-12-07 14:50:12 +0530

    feat : add FoodPreferenceTypeActivty UI

---
+++ app/src/main/java/com/ns/stellarjet/personalize/FoodPreferencesLaunchActivity.kt
@@ -0,0 +1,15 @@
+package com.ns.stellarjet.personalize
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.ns.stellarjet.R
+
+class FoodPreferencesLaunchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_preferences_launch)
+
+    }
+}
