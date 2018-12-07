f761ca9d57e212a6d3d15402791b16ae732b9c5d
Vignesh Prabhu <sekarvprabhu@gmail.com>
2018-12-07 16:17:48 +0530

    feat : add DrawerActvity UI

---
+++ app/src/main/java/com/ns/stellarjet/drawer/DrawerActivity.kt
@@ -0,0 +1,34 @@
+package com.ns.stellarjet.drawer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ns.stellarjet.R
+import kotlinx.android.synthetic.main.activity_drawer.*
+
+class DrawerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drawer)
+
+        button_drawer_close.setOnClickListener {
+            onBackPressed()
+        }
+
+        button_drawer_personal_assistance.setOnClickListener {
+            // TODO : contact number 180042577777
+        }
+
+        textView_drawer_boarding_pass.setOnClickListener {
+            //TODO launch Boarding Pass
+        }
+
+        textView_drawer_bookings.setOnClickListener {
+            //TODO launch Bookings
+        }
+
+        textView_drawer_preferences.setOnClickListener {
+            // TODO launch Preferences
+        }
+    }
+}
