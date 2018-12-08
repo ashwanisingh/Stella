7a3a331f93c92ed217209e6567dc992cfe60c53b
Vignesh Prabhu <sekarvprabhu@gmail.com>
2018-12-08 14:02:34 +0530

    feat : add BoardingPass List UI

---
+++ app/src/main/java/com/ns/stellarjet/drawer/BoardingPassActivity.kt
@@ -0,0 +1,13 @@
+package com.ns.stellarjet.drawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ns.stellarjet.R
+
+class BoardingPassActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_boarding_pass)
+    }
+}
