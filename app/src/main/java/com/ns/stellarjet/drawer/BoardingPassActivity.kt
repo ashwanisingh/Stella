247a4ae703cf4be18b3ae84d756c9b82ce3d6969
Vignesh Prabhu <sekarvprabhu@gmail.com>
2018-12-08 14:05:07 +0530

    feat : add BoardingPass back button

--- app/src/main/java/com/ns/stellarjet/drawer/BoardingPassActivity.kt
+++ app/src/main/java/com/ns/stellarjet/drawer/BoardingPassActivity.kt
@@ -3,11 +3,16 @@ package com.ns.stellarjet.drawer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ns.stellarjet.R
+import kotlinx.android.synthetic.main.activity_boarding_pass.*
 
 class BoardingPassActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_boarding_pass)
+
+        button_boarding_pass_back.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
