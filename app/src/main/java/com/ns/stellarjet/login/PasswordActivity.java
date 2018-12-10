f92f285949d5bfa52e375e23ec8adc48152e19c4
Vignesh Prabhu <sekarvprabhu@gmail.com>
2018-12-10 17:21:03 +0530

    feat : add DataBinding to PasswordActivity

--- app/src/main/java/com/ns/stellarjet/login/PasswordActivity.java
+++ app/src/main/java/com/ns/stellarjet/login/PasswordActivity.java
@@ -26,7 +26,6 @@ public class PasswordActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_password);
 
         ActivityPasswordBinding mActivityPasswordBinding = DataBindingUtil.setContentView(
                 this ,
