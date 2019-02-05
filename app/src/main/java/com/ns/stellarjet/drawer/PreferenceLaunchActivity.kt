3550640945f34a8de094961bfb2f91e2c5be4237
Vignesh Prabhu <sekarvprabhu@gmail.com>
2019-02-05 12:11:04 +0530

    feat : block manager list for secondary users

--- app/src/main/java/com/ns/stellarjet/drawer/PreferenceLaunchActivity.kt
+++ app/src/main/java/com/ns/stellarjet/drawer/PreferenceLaunchActivity.kt
@@ -3,9 +3,11 @@ package com.ns.stellarjet.drawer
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import com.ns.stellarjet.R
 import com.ns.stellarjet.personalize.FoodPreferencesLaunchActivity
+import com.ns.stellarjet.utils.SharedPreferencesHelper
 import kotlinx.android.synthetic.main.activity_preference_launch.*
 
 class PreferenceLaunchActivity : AppCompatActivity() {
@@ -16,6 +18,13 @@ class PreferenceLaunchActivity : AppCompatActivity() {
 
         button_preferences_back.setOnClickListener {
             onBackPressed()
+        }
+
+        val userType = SharedPreferencesHelper.getUserType(this@PreferenceLaunchActivity)
+        if(userType.equals("secondary" , true)){
+            textView_preferences_managers.visibility = View.INVISIBLE
+            textView_preferences_managers_help.visibility = View.INVISIBLE
+            view_preferences_managers.visibility = View.INVISIBLE
         }
 
         textView_preferences_managers.setOnClickListener {
